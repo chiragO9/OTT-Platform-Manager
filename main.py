e1a5dedc56cd3ec3b86e3a0d92a95c99af2e37c6
Chirag Solanki <solankichiragcs12@gmail.com>
2026-02-26 06:17:45 +0530

    add class attribute

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 class comedy_show:
 
+  platform_type = 'TV Show'
+  tax_rate = 18
+  total_shows = 0
+
+
   def __init__(self,name,channel,price,setup_box):
     self.name = name
     self.channel = channel
     self.price = price
     self.setup_box = setup_box
-
+    comedy_show.total_shows += 1 
   
   def show(self):
     return f"""
@@ -13,6 +18,8 @@ class comedy_show:
       channel is {self.channel}
       price is {self.price}
       setup box is {self.setup_box}
+      platform is {comedy_show.platform_type}
+      
     """
 
 
@@ -28,6 +35,11 @@ class comedy_show:
     else:
       return f"{self.channel} is regular channel"
 
+  def price_with_tax(self):
+    tax_amount = (self.price * comedy_show.tax_rate) / 100
+    final_price = self.price + tax_amount
+    return f"price after {comedy_show.tax_rate}% tax : {final_price} RS"
+
 x = comedy_show('friends','HBO',30,'tata sky')
 y = comedy_show('how I met your mothher','romedy',20,'tata sky')
 
@@ -38,3 +50,9 @@ print(x.is_premium_channel())
 print(y.show())
 print(x.apply_discount(20))
 print(x.is_premium_channel())
+
+print()
+
+print(f"platform type is {comedy_show.platform_type}")  
+print(f"tax is {comedy_show.tax_rate}")       
+print(f"Total shows {comedy_show.total_shows}")
